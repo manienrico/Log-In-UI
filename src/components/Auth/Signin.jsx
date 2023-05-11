fd4139fecbebda9af2c03ad9fb6a405e88e564b9
manienrico <87425895+manienrico@users.noreply.github.com>
2023-05-11 11:34:05 +0300

    Finished Log In, only mobile responsive left.

--- src/components/Auth/Signin.jsx
+++ src/components/Auth/Signin.jsx
@@ -2,15 +2,24 @@ import React, { useState } from 'react'
 import {Formik,Form,Field} from 'formik'
 import * as Yup from 'yup'
 import './auth.css'
+import pj from '../../images/peterJohn2.png'
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowRight,faEye,faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 
 function Signin() {
-  const [showPassword, setShowPassword] = useState(false)
+  const [toggleEye, setToggleEye] = useState(false)
 
   const handleClick = () => {
-    setShowPassword(!showPassword)
+    // passwordChange()
+    setToggleEye(!toggleEye)
+
+    // if(setToggleEye===true){
+    //   setPasswordType='text'
+    // }else{
+    //   setPasswordType('password')
+    // }
+    
   }
 
   const SigninSchema = Yup.object().shape({
@@ -21,72 +30,86 @@ function Signin() {
   return (
     <div className='app__signin'>
       <div className='app__signin-contain'>
+        
         <div className='app__signin-contain__left'>
-          <div className='app__signin-contain__left-top'>
-            <span>Logo Here</span>
-            <h5>Welcome back !!!</h5>
-          </div>
-          <h1>Log In</h1>
-          <Formik
-          validationSchema={SigninSchema}
-          onSubmit={(values)=>{
-            console.log(values)
-          }}
-          initialValues={{
-            email: "",
-            password: ""
-          }}
-          >
-            <Form>
-              <Field type="email" name="email" placeholder="Email">
-              {({
-                  field,
-                  form: {touched,errors},
-                  meta,
-                }) => (
-                  <div className='field__group'>
-                    <label htmlFor="email">Email</label>
-                    <input type='email' placeholder='Email' {...field} />
-                    {meta.touched && meta.error && (
-                      <div className='error'>{meta.error}</div>
-                    )}
-                  </div>
-                )}
-              </Field>
+          <div className='app__signin-contain__left-content'>
+            <div className='app__signin-contain__left-top'>
+              <span>Logo Here</span>
+              <h5>Welcome back !!!</h5>
+            </div>
+            <h1>Log In</h1>
+            <Formik
+            validationSchema={SigninSchema}
+            onSubmit={(values)=>{
+              console.log(values)
+            }}
+            initialValues={{
+              email: "",
+              password: ""
+            }}
+            >
+              <Form>
+                <Field type="email" name="email" placeholder="Email">
+                {({
+                    field,
+                    form: {touched,errors},
+                    meta,
+                  }) => (
+                    <div className='field__group'>
+                      <label htmlFor="email">Email</label>
+                      <input type='email' placeholder='Email' {...field} />
+                      {meta.touched && meta.error && (
+                        <div className='error'>{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
 
-              <Field type="password" name="password" placeholder="Password" >
-              {({
-                  field,
-                  form: {touched,errors},
-                  meta,
-                }) => (
-                  <div className='field__group'>
-                    <label htmlFor="password">Password</label>
-                    <input type='password' placeholder='Password' {...field}
-                     eye={
-                      <div className='eye__position'>
-                        {/* {
-                          showPassword === 'false' ? (<FontAwesomeIcon icon={faEye} style={{color: "rgba(255,166,201,1)",}} onClick={handleClick} />)
-                          : (<FontAwesomeIcon icon={faEyeSlash} style={{color: "#adc4eb",}} onClick={handleClick} />)
-                        } */}
-                        
+                <Field type="password" name="password" placeholder="Password" >
+                {({
+                    field,
+                    form: {touched,errors},
+                    meta,
+                  }) => (
+                    <div className='field__group'>
+                      <label htmlFor="password">Password</label>
+                      
+                      <div className='pwd__contain'>
+                        <input type={ toggleEye === false ? 'password':'text' }  placeholder='Password' {...field} />
+                        <sup>
+                          {/* <FontAwesomeIcon icon={faEye} style={{color: "rgba(255,166,201,1)",}} onClick={handleClick} /> */}
+                          {
+                            toggleEye === false ? (<FontAwesomeIcon icon={faEye} style={{color: "rgba(255,166,201,1)",}} onClick={handleClick} />)
+                            : (<FontAwesomeIcon icon={faEyeSlash} style={{color: "rgba(255,166,201,1)",}} onClick={handleClick} />)
+                          }
+                        </sup>
                       </div>
-                      }
-                      />
-                      <FontAwesomeIcon icon={faEye} style={{color: "rgba(255,166,201,1)",}}  />
-                    {meta.touched && meta.error && (
-                      <div className='error'>{meta.error}</div>
-                    )}
-                  </div>
-                )}
-              </Field>
-              <button type="submit">LOGIN IN<FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff", paddingLeft: '0.5rem'}} /></button>
-            </Form>
-          </Formik>
+                      
+                        
+                      {meta.touched && meta.error && (
+                        <div className='error'>{meta.error}</div>
+                      )}
+                    </div>
+                  )}
+                </Field>
+                <button type="submit">LOGIN IN<FontAwesomeIcon icon={faArrowRight} style={{color: "#ffffff", paddingLeft: '0.5rem'}} /></button>
+              </Form>
+            </Formik>
+          </div>
+          
+          {/* <div className='app__signin-contain__left-pic'>
+            <img src={pj} alt="Peter and John" />
+          </div> */}
           {/* <span className='form__link'>Have no account <a href="signup">Create Account</a></span> */}
         </div>
-        <div className='app__signin-contain__right'></div>
+        {/* <div className='app__signin-contain__between'>
+          
+        </div> */}
         
+        <div className='app__signin-contain__right'></div>
+        <div className='app__signin-contain__picture'>
+          <img src={pj} alt="Peter and John" />
+        </div>
       </div>
     </div>
   )
