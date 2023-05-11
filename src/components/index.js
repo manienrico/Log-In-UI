c155b112d7c327a80f64b38e42c13a3e92a84893
manienrico <87425895+manienrico@users.noreply.github.com>
2023-05-11 17:21:05 +0300

    working on mobile responsiveness

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,2 @@
 export {default as Signup} from './Auth/Signup'
 export {default as Signin} from './Auth/Signin'
-// export {default as Signup} from './Auth/Signup'
-// export {default as Signup} from './Auth/Signup'
-// export {default as Signup} from './Auth/Signup'
-// export {default as Signup} from './Auth/Signup'
-// export {default as Signup} from './Auth/Signup'
